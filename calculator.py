4c13b7c8ff578aeb30d566c7dec3950086e08390
Alize <pusikentut@gmail.com>
2024-07-19 22:00:51 +0700

    membuat fungsi bagi

--- calculator.py
+++ calculator.py
@@ -2,3 +2,4 @@ from tambah import *
 
 tambah(2,2)
 kurang(2,1)
+bagi(400,100,2,2)
--- tambah.py
+++ tambah.py
@@ -9,3 +9,12 @@ def kurang(*args):
     for data in args:
         hasil -= data
     print(hasil)
+
+def bagi(*args):
+    try:
+        hasil=args[0]
+        for data in args[1:]:
+            hasil /= data
+        print(hasil)
+    except ZeroDivisionError:  
+        print("tidak bisa membagi dengan 0")
