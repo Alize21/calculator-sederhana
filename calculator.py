ef7246908892bf39f0d30bac6a6688d10359f7b0
Alize <pusikentut@gmail.com>
2024-07-21 22:58:08 +0700

    menambahkan file geometry di branch geometry

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 from tambah import *
+from geometry import *
 
 tambah(2,2)
 kurang(2,1)
 kali(2,3,2)
 bagi(400,100,2)
 pangkat(2,4)
+luas_persegi_panjang(4,5)
+keliling_persegi_panjang(4,5)
+lebar_persegi_panjang(260,20)
---
+++ geometry.py
@@ -0,0 +1,18 @@
+
+def luas_persegi_panjang(p:int,l:int)->int:
+
+    '''input: panjang dan lebar persegi dalam satuan cm'''
+
+    print(f'luas persegi panjang ini adalah {p*l} cm')
+
+def keliling_persegi_panjang(p:int,l:int)->int:
+
+    '''input: panjang dan lebar persegi dalam satuan cm'''
+
+    print(f'keliling persegi panjang ini adalah {2*(p+l)} cm')
+
+def lebar_persegi_panjang(L:int,p:int)->int:
+
+    '''input: luas dan panjang persegi dalam satuan cm'''
+
+    print(f'keliling persegi panjang ini adalah {L/p} cm')
